cf83b59275ccd35728b10c22d30d7f222f7fbd0e
Flower804 <146494346+Flower804@users.noreply.github.com>
2024-05-20 09:57:10 +0100

    added updated python code
    Recovered python code with the csv file protocol
    Still in the process of recovering the code with the 3 process
    Processes
    1 - 2way comm with csv file handling 
    2 - camera protocol
    3 - safeguard protocol

--- bbt.py
+++ bbt.py
@@ -1,9 +1,11 @@
 #PY imports
 import serial
+import csv
 import os
 import time
 import multiprocessing as mp
 from datetime import datetime
+
 #cansat imports
 import adafruit_bmp3xx
 from picamera import PiCamera
@@ -13,12 +15,21 @@ import board
 #handlers
 force = True
 Turn  = False
+data = ("milisec", "acel_x", "acel_y", "acel_z", "acel_tot", "temp", "pressure", "humidity", "altitude", "lumi", "cur_state", "gps_alt", "vel", "dir")
 
-
+#first time deff/code and log sistem for debugging prop
+filename = ('log.txt') #action log sistem
+provser = ('provser.txt') #ser log sistem
+f = open(filename, 'a')
+s = open(provser, 'a')
+#def csv file
+with open('datasheet.csv', 'a', newline='') as csvfile:
+    writer = csv.writer(csvfile, delimiter= ' ', quotechar = '|')
+    
 #Serial Stuff + Board stuff
 #ser = serial.Serial('COM5', 9600, timeout=5) #using COM5 for testing purposes on laptop only
 ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=5) #using /dev/ttyACM0- serial comm for rasp usb connection to esp
-conectsec = ser.readline()
+conectsec = ser.readline() #secure the conection to circumvent false starts
 
 i2c = board.I2C()
 bmp = adafruit_bmp3xx.BMP3XX_I2C(i2c)
@@ -30,31 +41,46 @@ def get_time(): #get current time for multi use
 
 def info_dump():
     f = open(filename, 'a')
+    s = open(provser, 'a') #is this really necessary? :\
+    ser_list = [] #WHY AREN'T YOU OK???'
     f.write("-------" + get_time() + "-------" + '\n' + '\n')
     f.close()
     while(1):
         f = open(filename, 'a')
         input_str = ser.readline().decode("utf-8").strip()
         if input_str == "sent_complete":
+            serv = ' '.join(ser_list)
+            with open('testopt.csv', 'a', newline= '') as csvfile: #honestly I'm  surprised this works
+                writer = csv.writer(csvfile, delimiter= ' ', quotechar = '|')
+                writer.writerow([ser_list])   
             ser.write(b'done\n')
+            print("csv done")
             return 0
         else:
-            f.write(input_str + '\n')
+            f.write("input" + "!" + input_str + '\n')
             f.close()
+            inputser = tupleconv(input_str)
+            ser_list.append(inputser)
+            print("data received")
             
+def tupleconv(tup): #Done: i think this is necessary 10.1.2.116
+    str = ''        #Yup it is
+    for item in tup:
+        str = str + item
+    return str #this does magic
 
 filename = ('sensors_data.txt')
 f = open(filename, 'a')
-def communication_protocol():
+def communication_protocol(): #serial controler for 2way comm betwhen esp32 and RP
     print("camera process initiated")
     while(1):
         input_str = ser.readline().decode("utf-8").strip()
         print(input_str) #TODO only for debuggin purposes
         if input_str == "ready" or force == True: #force for when comm is stuck or desicronissed
-            while(True):
+            while(True): 
                 ser.write(b'on\n') #testing purposes
                 input_str = ser.readline().decode("utf-8").strip()
-                if(input_str == "received"):
+                if(input_str == "received"): #start csv data sheet
                     info_dump()
                     break
         elif(input_str == "."):
@@ -63,7 +89,7 @@ def communication_protocol():
             ser.write(b'done\n')
 
 
-def camera_protocol():
+def camera_protocol(): #the name says it all
     a = 0
     cam = PiCamera()
     cam.resolution = (360,260)
@@ -71,7 +97,7 @@ def camera_protocol():
     while(1):
         a = a + 1 
         cam.start_preview()
-        cam.capture('/home/admin/fotos/foto%s.jpg' % a)
+        cam.capture('/home/admin/fotos/foto%s.jpg' % a) #save photo file in format foto + photo number + .jpg
         print(bmp.pressure)
         print(bmp.temperature)
         print(bmp.altitude)
@@ -80,8 +106,8 @@ def camera_protocol():
         cam.stop_preview()
         time.sleep(1)
         
-if __name__ == "__main__":
-    print("task st")
+if __name__ == "__main__": #multiprocess controler
+    print("process started")
     p1 = mp.Process(target=communication_protocol, args=(10, )) #def multprocss for comm prot
     p2 = mp.Process(target=camera_protocol, args=(10, )) #def multprocess for cam prot
     p1.start() #start comm
